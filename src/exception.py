645f3ac96fc9e3985f3743c87eb3c5b0b1e5a7e5
Niral Patel <nir64.au@gmail.com>
2026-01-10 18:07:52 -0600

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
 from src.logger import logging
 
-def error_message_details(error, error_detail:sys):
+def error_message_details(error, error_detail):
     _, _, exc_tb = error_detail.exc_info()
     if exc_tb is not None:
         file_name = exc_tb.tb_frame.f_code.co_filename
@@ -13,7 +13,7 @@ def error_message_details(error, error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_detail=error_detail)
         
